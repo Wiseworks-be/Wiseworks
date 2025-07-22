9c7ee0e79ab79f815ae8c65bee10a10b97ffbd3e
rayo254254 <r1019767@student.thomasmore.be>
2025-07-22 17:51:14 +0200

    test

--- project-4-carbone-doc-handler/ryan.py
+++ project-4-carbone-doc-handler/ryan.py
@@ -1,1 +1,5 @@
 print("ryan omondi")
+if __name__ == "__main__":
+    print("This is the ryan.py file in the project-4-carbone-doc-handler directory.")   
+    else:
+        print("This is not the ryan.py file.")
